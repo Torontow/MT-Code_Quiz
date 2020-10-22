79727d84c5ebf3af76210161ba7ca265a1c24ede
Torontow <mtorontow0@gmail.com>
2020-10-22 15:59:07 -0400

    Pseudocode and slight debug
    Timer wasn't stopping at 0 so altered the comparison operator.
    Translated all of my handwritten pseudocode into comments in an effort to make my process more organized.

--- script.js
+++ script.js
@@ -1,22 +1,51 @@
+// Starting variables
+var startEl = document.getElementById("start");
+
+// Timer variables
 var timeLeft = 0;
 var countEl = document.getElementById("count");
-var startEl = document.getElementById("start");
-console.log(countEl);
 
+// Sets a timer for the quiz. 
 function startTimer() {
     timeLeft = 60;
     var timer = setInterval(function() {
         timeLeft--; 
         countEl.textContent = timeLeft;
 
-            if (timeLeft === 0) {
+            if (timeLeft == 0) {
                 clearInterval(timer);
                 // gameOver();
             }
         }, 1000);
 }
 
-startEl.addEventListener("click", startTimer);
+function quizMe() {
+    console.log("run() ran.");
+// -while (timeLeft > 0) {
+    // -for loop for number of questions: 
+        // -show a question from a list and put it in question text field
+        // -show multiple choice answers to corresponding question (answer div)
+        // -when an answer is clicked
+            // -if correct (true): show card with "correct" styling for 1 second
+            // -else subtract 3 from timeLeft and display "incorrect" card for 1 second
+    // -clearInterval(timer)
+    // -console.log(timeLeft)
+    gameOver();
+}
+
+function gameOver() {
+    console.log("gameOver() ran.");
+// -Displays your score
+// -asks to enter initials for score
+// -updates leaderboard content
+}
+
+// Start Button 
+startEl.addEventListener("click",startTimer);
+startEl.addEventListener("click",quizMe);
+
+
+    
 
 
 
